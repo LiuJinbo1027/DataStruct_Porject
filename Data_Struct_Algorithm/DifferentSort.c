bed32849a8278a00e4d6d5be15975bac0caedb13
刘谨搏 <1753809506@qq.com>
2025-10-31 14:37:46 +0800

    不同的排序算法

--- Data_Struct_Algorithm/DifferentSort.c
+++ Data_Struct_Algorithm/DifferentSort.c
@@ -76,3 +76,21 @@ void shellSort(int arr[], int n) {
         }
     }
 }
+
+// ========== 选择排序实现 ==========
+void selectSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
