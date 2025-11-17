7c2673eb55e40fcd88dbcf94ed5c7b18c73df043
刘谨搏 <1753809506@qq.com>
2025-11-17 16:36:44 +0800

    归并排序

--- Data_Struct_Algorithm/mergeSort.c
+++ Data_Struct_Algorithm/mergeSort.c
@@ -15,19 +15,21 @@ void merge(int arr[], int left, int mid, int right, int temp[]) {
         }
     }
 
+    // 考虑到两数组长度不等的情况
     while (i <= mid) {
         temp[k++] = arr[i++];
     }
     while (j <= right) {
         temp[k++] = arr[j++];
     }
-
+    // 用有序数组temp替换原来的两个待归并数组
     k = 0;
     while (left <= right) {
         arr[left++] = temp[k++];
     }
 }
 
+// 自底向顶的递归逻辑：先将镇整个无序数组分成单一元素，再将单一元素两两排序，四四排序，直到从下至上完成排序
 void mergeSortRecursive(int arr[], int left, int right, int temp[]) {
     if (left < right) {
         int mid = left + (right - left) / 2;
